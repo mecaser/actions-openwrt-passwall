a5ca01735f5352cbf0f47a91b2cfc6bfbcbd2374
mecaser <evzs@163.com>
2020-05-14 19:38:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,4 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/10.10.10.1/10.10.10.254/g' package/base-files/files/bin/config_generate
+# sed -i 's/10.10.10.1/10.10.10.254/g' package/base-files/files/bin/config_generate
